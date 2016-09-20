40b91b4716cb2361457f87d4e6308fe0ad0d02d7
TechDetective <vishwajeetsinhjadeja4@gmail.com>
2016-09-20 11:05:24 +0530

    Minor Change

--- app.js
+++ app.js
@@ -7,12 +7,13 @@ var bodyParser = require('body-parser');
 var passport = require('passport');
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var dotenv = require('dotenv');
+var dotenv = require('dotenv').config();
 var bluebird = require('bluebird');
 var mongoose = require('mongoose');
 var session = require('cookie-session');
 
 mongoose.Promise = bluebird;
+console.log(process.env.MONGO_URL);
 mongoose.connect(process.env.MONGO_URL);
 
 var app = express();
